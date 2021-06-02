044465fcb83dc4a699bdcb64aaaeac68cdb319e1
Sebastian <sebastian.gulino@widergy.com>
2021-06-02 09:45:20 -0300

    fix: refactor component methods

--- src/app/screens/Calculadora/index.js
+++ src/app/screens/Calculadora/index.js
@@ -1,96 +1,50 @@
-import React,{useState} from 'react'
-import {View, TextInput} from 'react-native'
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import styles from './styles'
-import Button from './components/Button'
-import {BUTTONS} from './constants'
+import React, {useState} from 'react';
+import {View, TextInput} from 'react-native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
-const Calculadora =()=>{
-    //Variables y funciones
-    let [display,setDisplay]=useState('')
-    const leerPresionado=(target)=>{
-        GET_BUTTONS.find(button=>button.label===target).action()
-    }
-    const setExpression=(expression)=>{
-        console.log(display)
-        expression!=null
-            ?   setDisplay(display.concat(expression))
-            :   setDisplay('')
-    }
-    const deleteLastValue=()=>{
-        setDisplay(display.slice(0,-1))
-    }
-    const operation=(op,numberOne,numberTwo)=>{
-        let result
-        switch (op) {
-            case '+':
-                result=numberOne+numberTwo
-                break;
-            case '-':
-                result=numberOne-numberTwo
-                break;
-            case '*':
-                result=numberOne*numberTwo
-                break;
-            case '/':
-                if(numberTwo!=0){
-                    result=numberOne/numberTwo
-                }else{
-                    console.warn("Indeterminado")
-                    setDisplay('')
-                    return ''
-                }
-                break;
-            case '%':
-                result=numberOne%numberTwo
-                break;
-        }
-        return result
-    }
-    const solve=()=>{
-        let result=''
-        let exp=RegExp(/^(-?(\d*\,)?\d*)([\+|\-|\*|\/|\%]{1})(-?(\d*\,)?\d*)$/)
-        if(display.match(exp) != null){
-            result=display.replace(/\,/g,'.')
-            let consultExpression=result.split(/([\+|\-|\*|\/|\%]{1})/)
-            console.log(consultExpression)
-            consultExpression.length==3
-                ?   setDisplay(operation(consultExpression[1],parseFloat(consultExpression[0]),parseFloat(consultExpression[2])).toLocaleString("es-ES").replace('.',','))
-                :   "Faltan los casos de operaciones con números negativos"
-        }else {
-            console.warn("Error:No es una operación valida.")
-            setDisplay('')
-        }
-    }
-    let GET_BUTTONS=BUTTONS(setExpression,deleteLastValue,solve)
-    //Fin de variables y funciones
-    return (
-        <KeyboardAwareScrollView style={styles.contain}>
-            <View style={styles.containerCalculadora}>
-                <View style={styles.boxPantalla}>
-                    <View style={styles.pantalla}>
-                        <TextInput
-                        onKeyPress={({nativeEvent})=>{
-                            (nativeEvent.key.match(/[0123456789]/)||nativeEvent.key.match(/[+-/=%*,]/))!=null
-                            ?   leerPresionado(nativeEvent.key)
-                            :   nativeEvent.key=='Backspace' 
-                                ?   GET_BUTTONS.find(button=>button.label=="<").action()
-                                :   console.warn("Introduzca números o caracteres de una calculadora.")
-                        }}
-                        style={styles.textResultado}>{display}</TextInput>
-                    </View>
-                </View>
-                <View style={styles.boxButtons}>
-                {
-                    GET_BUTTONS.map((button,key)=>{
-                        return (
-                            <Button key={key} button={button}/>
-                        )
-                    })
-                }
-                </View>
-            </View>
-        </KeyboardAwareScrollView>
-    )
-}
-export default Calculadora
+import styles from './styles';
+import Button from './components/Button';
+import { retrieveButtons } from './utils';
+
+const Calculadora = () => {
+  //Variables y funcionesa
+  const [display, setDisplay] = useState('');
+
+  const leerPresionado = target => {
+    GET_BUTTONS.find(button => button.label === target).action();
+  };
+
+  let GET_BUTTONS = retrieveButtons(display, setDisplay);
+
+  //Fin de variables y funciones
+  return (
+    <KeyboardAwareScrollView style={styles.contain}>
+      <View style={styles.containerCalculadora}>
+        <View style={styles.boxPantalla}>
+          <View style={styles.pantalla}>
+            <TextInput
+              onKeyPress={({nativeEvent}) => {
+                (nativeEvent.key.match(/[0123456789]/) ||
+                  nativeEvent.key.match(/[+-/=%*,]/)) != null
+                  ? leerPresionado(nativeEvent.key)
+                  : nativeEvent.key == 'Backspace'
+                  ? GET_BUTTONS.find(button => button.label == '<').action()
+                  : console.warn(
+                      'Introduzca números o caracteres de una calculadora.',
+                    );
+              }}
+              style={styles.textResultado}>
+              {display}
+            </TextInput>
+          </View>
+        </View>
+        <View style={styles.boxButtons}>
+          {GET_BUTTONS.map((button, key) => {
+            return <Button key={key} button={button} />;
+          })}
+        </View>
+      </View>
+    </KeyboardAwareScrollView>
+  );
+};
+export default Calculadora;
--- src/app/screens/Calculadora/utils.js
+++ src/app/screens/Calculadora/utils.js
@@ -1,3 +1,154 @@
-export const solve=(expression)=>{
-    console.log(expression)
-}
+export const getSetExpression = (display, setDisplay) => expression => {
+  console.log(display);
+  expression != null ? setDisplay(display.concat(expression)) : setDisplay('');
+};
+
+const operation = (setDisplay, op, numberOne, numberTwo) => {
+  let result;
+  switch (op) {
+    case '+':
+      result = numberOne + numberTwo;
+      break;
+    case '-':
+      result = numberOne - numberTwo;
+      break;
+    case '*':
+      result = numberOne * numberTwo;
+      break;
+    case '/':
+      if (numberTwo != 0) {
+        result = numberOne / numberTwo;
+      } else {
+        console.warn('Indeterminado');
+        setDisplay('');
+        return '';
+      }
+      break;
+    case '%':
+      result = numberOne % numberTwo;
+      break;
+  }
+  return result;
+};
+
+export const getSolve = (display, setDisplay) => () => {
+  let result = '';
+  let exp = RegExp(/^(-?(\d*\,)?\d*)([\+|\-|\*|\/|\%]{1})(-?(\d*\,)?\d*)$/);
+  if (display.match(exp) != null) {
+    result = display.replace(/\,/g, '.');
+    let consultExpression = result.split(/([\+|\-|\*|\/|\%]{1})/);
+    console.log(consultExpression);
+    consultExpression.length == 3
+      ? setDisplay(
+          operation(
+            setDisplay,
+            consultExpression[1],
+            parseFloat(consultExpression[0]),
+            parseFloat(consultExpression[2]),
+          )
+            .toLocaleString('es-ES')
+            .replace('.', ','),
+        )
+      : 'Faltan los casos de operaciones con números negativos';
+  } else {
+    console.warn('Error:No es una operación valida.');
+    setDisplay('');
+  }
+};
+
+export const getDeleteLastValue = (display, setDisplay) => () => {
+  setDisplay(display.slice(0, -1));
+};
+
+export const retrieveButtons = (display, setDisplay) => {
+  const setExpression = getSetExpression(display, setDisplay);
+  const deleteLastValue = getDeleteLastValue(display, setDisplay);
+  const solve = getSolve(display, setDisplay);
+
+  return [
+    {
+      label: 'C',
+      action: () => setExpression(null),
+      style: 'del',
+    },
+    {
+      label: '<',
+      action: () => deleteLastValue(),
+      style: 'del',
+    },
+    {
+      label: '%',
+      action: () => setExpression('%'),
+      style: 'operation',
+    },
+    {
+      label: '/',
+      action: () => setExpression('/'),
+      style: 'operation',
+    },
+    {
+      label: '7',
+      action: () => setExpression('7'),
+    },
+    {
+      label: '8',
+      action: () => setExpression('8'),
+    },
+    {
+      label: '9',
+      action: () => setExpression('9'),
+    },
+    {
+      label: '*',
+      action: () => setExpression('*'),
+      style: 'operation',
+    },
+    {
+      label: '4',
+      action: () => setExpression('4'),
+    },
+    {
+      label: '5',
+      action: () => setExpression('5'),
+    },
+    {
+      label: '6',
+      action: () => setExpression('6'),
+    },
+    {
+      label: '-',
+      action: () => setExpression('-'),
+      style: 'operation',
+    },
+    {
+      label: '1',
+      action: () => setExpression('1'),
+    },
+    {
+      label: '2',
+      action: () => setExpression('2'),
+    },
+    {
+      label: '3',
+      action: () => setExpression('3'),
+    },
+    {
+      label: '+',
+      action: () => setExpression('+'),
+      style: 'operation',
+    },
+    {
+      label: '0',
+      action: () => setExpression('0'),
+    },
+    {
+      label: ',',
+      action: () => setExpression(','),
+    },
+    {
+      label: '=',
+      action: () => solve(),
+      style: 'operation',
+    },
+  ];
+};
