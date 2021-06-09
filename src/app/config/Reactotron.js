86be2c5d615b74b3aa1992c068f4443bc27c92f1
alcurvelo <alberto.curvelo@widergy.com>
2021-06-09 18:39:26 -0300

    Reactotron suscripcion con redux agregada.

--- src/app/config/Reactotron.js
+++ src/app/config/Reactotron.js
@@ -1,10 +1,12 @@
 import Reactotron from 'reactotron-react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {reactotronRedux} from 'reactotron-redux';
 
-Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
+const reactotron = Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
   .configure({
     name: 'My Firts App',
   })
+  .use(reactotronRedux())
   .useReactNative({
     asyncStorage: false, // there are more options to the async storage.
     networking: {
@@ -16,3 +18,5 @@ Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either com
     overlay: false, // just turning off overlay
   })
   .connect();
+
+export default reactotron;
--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,13 +1,17 @@
-import {applyMiddleware, createStore, combineReducers} from 'redux';
+import {applyMiddleware, createStore, combineReducers, compose} from 'redux';
 import thunk from 'redux-thunk';
 
 import {historyReducer} from '../redux/history/reducer';
 import {authReducer} from '../redux/auth/reducer';
+import Reactotron from '../config/Reactotron';
 
 const reducers = combineReducers({
   historyReducer,
   authReducer,
 });
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const store = createStore(
+  reducers,
+  compose(applyMiddleware(thunk), Reactotron.createEnhancer()),
+);
 export default store;
