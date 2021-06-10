49e6420771e0d4549a6935f5d292f594639bb457
alcurvelo <alberto.curvelo@widergy.com>
2021-06-10 19:58:00 -0300

    Redux-recompose e Immutable añadido, modificacion de reducer, actions.

--- src/app/redux/history/reducer.js
+++ src/app/redux/history/reducer.js
@@ -1,20 +1,20 @@
-export const initialState = {
-  history: [],
-};
+import {completeReducer, createReducer} from 'redux-recompose';
+import {actions} from './actions';
+import Immutable from 'seamless-immutable';
 
-export function historyR(state = initialState, action) {
-  switch (action.type) {
-    case 'GET_HISTORIES':
-      return {
-        ...state,
-        history: state.history,
-      };
-    case 'SET_HISTORY':
-      return {
-        ...state,
-        history: [...state.history, action.payload],
-      };
-    case 'EDIT_EXPRESSION_HISTORY': {
+const initialState = {
+  history: [],
+  historyFailure: null,
+  historyLoading: false,
+};
+const historyReducers = {
+  primaryActions: actions.primaryActions,
+  override: {
+    [actions.DEL_ALL_SUCCESS]: (state, action) =>
+      Immutable.merge(state, {history: action.payload}),
+    [actions.SET_HISTORY_SUCCESS]: (state, action) =>
+      Immutable.merge(state, {history: [...state.history, action.payload]}),
+    [actions.EDIT_EXPRESSION_HISTORY_SUCCESS]: (state, action) => {
       const {newExpression, id} = action.payload;
       let historyCopy = [...state.history];
       historyCopy[id] = newExpression;
@@ -22,8 +22,8 @@ export function historyR(state = initialState, action) {
         ...state,
         history: historyCopy,
       };
-    }
-    case 'DELETE_HISTORY_FOR_ID': {
+    },
+    [actions.DELETE_HISTORY_FOR_ID_SUCCESS]: (state, action) => {
       const historyFiltered = state.history.filter(
         (expression, key) => key !== action.payload,
       );
@@ -31,14 +31,8 @@ export function historyR(state = initialState, action) {
         ...state,
         history: historyFiltered,
       };
-    }
-    case 'DELETE_ALL': {
-      return {
-        ...state,
-        history: [],
-      };
-    }
-    default:
-      return state;
-  }
-}
+    },
+  },
+};
+
+export default createReducer(initialState, completeReducer(historyReducers));
--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,7 +1,7 @@
 import {applyMiddleware, createStore, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 
-import {historyR} from '../redux/history/reducer';
+import historyR from './history/reducer';
 
 const reducers = combineReducers({
   historyR,
