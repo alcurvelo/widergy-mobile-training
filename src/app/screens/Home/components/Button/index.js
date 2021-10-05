d7607e14c03acd256a016a815b68258c1e1f2981
alcurvelo <alberto.curvelo@widergy.com>
2021-10-05 13:13:57 -0300

    Agrupando ramas

--- src/app/screens/Home/components/Button/index.js
+++ src/app/screens/Home/components/Button/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {View, Text, TouchableHighlight} from 'react-native';
+import { View, Text, TouchableHighlight } from 'react-native';
 import styles from './styles.js';
 
-const Button = ({label, onPress, style, variantStyle}) => {
+const Button = ({ label, onPress, style, variantStyle }) => {
   return (
     <TouchableHighlight
       activeOpacity={0.45}
       underlayColor="#4bcaf9"
       onPress={onPress}
-      style={[styles.boxButton, variantStyle]}>
+      style={[styles.boxButton, variantStyle]}
+    >
       <View style={[styles.button, style]}>
         <Text style={styles.textButton}>{label}</Text>
       </View>
